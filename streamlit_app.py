cf244f8959b586061f74a59da1adea216ff73713
JulianTorrest <43328220+JulianTorrest@users.noreply.github.com>
2024-05-09 09:07:08 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,13 +4,14 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 
 # URL cruda del archivo CSV en GitHub
-file_url = 'https://raw.githubusercontent.com/JulianTorrest/prueba_tecnica_a/main/vaccines/locations.csv'
+file_url1 = 'https://raw.githubusercontent.com/JulianTorrest/prueba_tecnica_a/main/vaccines/locations.csv'
 
 # Cargar los datos desde la URL
-locations_vaccines_df = pd.read_csv(file_url)
+locations_vaccines_df = pd.read_csv(file_url1)
 
 # URL cruda del archivo CSV en GitHub
-file_url = 'https://raw.githubusercontent.com/JulianTorrest/prueba_tecnica_a/main/vaccines/vaccinations-by-manufacturer.csv'
+file_url2 = 'https://raw.githubusercontent.com/JulianTorrest/prueba_tecnica_a/main/vaccines/vaccinations-by-manufacturer.csv'
 
 # Cargar los datos desde la URL
-vaccinations-by-manufacturer_df = pd.read_csv(file_url)
+vaccinations_by_manufacturer_df = pd.read_csv(file_url2)
+
