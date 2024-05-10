0ad3c2f9ec6d722dfc14107f47aa6b6765d32740
JulianTorrest <43328220+JulianTorrest@users.noreply.github.com>
2024-05-09 22:35:46 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -388,5 +388,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error
 
-print(vaccines_country_data_df.columns)
+st.write("Columnas del DataFrame vaccines_country_data_df:")
+st.write(vaccines_country_data_df.columns.tolist())
+
 
